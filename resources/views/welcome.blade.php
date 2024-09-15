606f45afe62489341426b28db0eea52f80007a6c
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2024-09-15 21:35:25 +0200

    Add WireUiCompoent

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
+    <wireui:scripts />
 
     <!-- Styles -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
