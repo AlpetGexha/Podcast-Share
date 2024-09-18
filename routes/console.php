448b3988a9d34cc6ea98876368d92dccddbb8741
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2024-09-19 01:14:31 +0200

    Add Schedule for end of Party

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Models\ListeningParty;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+// if the party has ended deactivate that party
+Schedule::call(function () {
+    ListeningParty::where('end_at', '<', now())->update(['is_active' => false]);
+})->everyMinute();
