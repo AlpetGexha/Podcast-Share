88b205408f43072b2a7f2466a63e86083faf1e94
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2024-09-18 20:47:00 +0200

    WIP

--- resources/views/livewire/pages/parties/show.blade.php
+++ resources/views/livewire/pages/parties/show.blade.php
@@ -3,18 +3,17 @@
 use Livewire\Volt\Component;
 use App\Models\ListeningParty;
 new class extends Component {
-
     public ListeningParty $listeningParty;
-
     public function mount(ListeningParty $listeningParty)
     {
+        // $listeningParty->load('episode');
         $this->listeningParty = $listeningParty;
     }
 }; ?>
-<div class="text-red-600">
-    {{ $listeningParty->title }}
+<div>
+    {{ $listeningParty }}
+    {{ $listeningParty->episode->title }}
     {{ $listeningParty->start_at }}
-    {{-- {{ $listeningParty->episode->title }} --}}
     {{-- {{ $listeningParty->episode->podcast->title }} --}}
     {{-- <img src="{{ $listeningParty->episode->podcast->artwork_url }}" class="size-28" /> --}}
 </div>
